99becefbf33beb99e519318f826a438fab8983ad
Yadlapati Yaswanth <yadlapatiyaswanth1@gmail.com>
2026-08-08 11:34:09 +0530

    Add files via upload

---
+++ exp 24/input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c; /*variable declaration*/
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+c=a+b; //adding two numbers
+printf("sum is %d",c);
+return 0;
+}
---
+++ exp 24/output.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c; 
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+c=a+b; 
+printf("sum is %d",c);
+return 0;
+}
